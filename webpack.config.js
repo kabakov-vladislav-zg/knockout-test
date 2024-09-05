f08bea0febfc64e19c0c1bd878e727b678184342
vladislav <kabakov.vladislav@adinadin.ru>
2024-09-05 08:09:10 +0400

    feat: gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = {
 	entry: './src/main.js',
 	output: {
-		path: __dirname + '/build',
+		path: __dirname + '/dist',
 		filename: 'bundle.js'
 	},
   plugins: [
